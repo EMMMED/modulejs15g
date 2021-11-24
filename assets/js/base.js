5b64524da2a3d102d450b64122d66411bc48585f
EMMMED <personal.emedina@gmail.com>
2021-11-23 22:23:58 -0600

    excercise 2, class 1

--- assets/js/base.js
+++ assets/js/base.js
@@ -7,15 +7,26 @@
 	Imprimir el resultado en consola
 */
 
-let number1 = parseInt(prompt("Introduce el valor de a"));
-let number2 = parseInt(prompt("Introduce el valor de b"));
+const number1 = parseInt(prompt("Introduce el valor de a"));
+const number2 = parseInt(prompt("Introduce el valor de b"));
 
 if(isNaN(number1) || isNaN(number2)){
-    console.log("Error, favor de introducir un numero valido")
+    console.error("Error, favor de introducir un numero valido")
 }else if(number1 > number2){
-    console.log(number1 + " entre "+ number2 + " = ", number1/number2)
+    console.log(number1 + " entre "+ number2 + " = ", number1/number2);
 }else if(number1 < number2){
-    console.log(number1 + " mas " + number2 + " = " , number1+number2)
+    console.log(number1 + " mas " + number2 + " = " , number1+number2);
 }else{
-    console.log(number1 + " multiplicado por  " + number2 + " = ", number1*number2)
+    console.log(number1 + " multiplicado por  " + number2 + " = ", number1*number2);
+}
+
+/*
+Tablas de todos los numeros del 1 al 10 
+*/
+
+for(let i = 1; i <= 10; i++){
+    console.log(`Tabla del ${i}`)
+    for(let j = 1 ; j <= 10; j++){
+    console.log(`${i} * ${j} = ${i*j}`)
+    }
 }
