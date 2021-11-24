377bbfd7db6ca27065a6e4865ec9446fd2facd7a
EMMMED <personal.emedina@gmail.com>
2021-11-23 21:18:38 -0600

    first excercise, first class

--- assets/js/base.js
+++ assets/js/base.js
@@ -6,3 +6,16 @@
 	si a == b multiplicar ambos numeros
 	Imprimir el resultado en consola
 */
+
+let number1 = parseInt(prompt("Introduce el valor de a"));
+let number2 = parseInt(prompt("Introduce el valor de b"));
+
+if(isNaN(number1) || isNaN(number2)){
+    console.log("Error, favor de introducir un numero valido")
+}else if(number1 > number2){
+    console.log(number1 + " entre "+ number2 + " = ", number1/number2)
+}else if(number1 < number2){
+    console.log(number1 + " mas " + number2 + " = " , number1+number2)
+}else{
+    console.log(number1 + " multiplicado por  " + number2 + " = ", number1*number2)
+}
