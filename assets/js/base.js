8cfd8e24644974c1b39cfa68248f8bf9b7a771e1
EMMMED <personal.emedina@gmail.com>
2021-11-29 22:13:59 -0600

    fifth class

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,76 +1,121 @@
-let saludo ='hola'
+// // Funcion con parametros
 
-for( i=0 ; i<= saludo.length; i++){
-    console.log(saludo[i])
+// function suma (a,b) {
+//     console.log(`la suma de ${a} más ${b} es igual a ${a+b}`)
+// }
+
+// suma(10, 20)
+
+// //Funcion sin parametros
+
+// function multiplicar () {
+//     console.log(2 * 2)
+// }
+
+// multiplicar()
+
+// function toCelsius(fahrenheit) {
+//     let celsiusGrades = ( ( 5 / 9 ) * ( fahrenheit -32 ) )
+
+//     return
+// }
+
+// let celsiusGradesReturned = toCelsius(10)
+
+// console.log(celsiusGradesReturned)
+
+
+//Funcion que pude el nombre y lo imprime en consola
+
+
+// function nameConsole(nameFunction){
+//     console.log(nameFunction)
+// }
+
+
+// const superName = prompt("Introduce tu nombre")
+
+// nameConsole(superName)
+
+// let nameReversed = ""
+
+
+// function reversed(nameToReverse){
+//     let newWord = ""
+//     for(let i = nameToReverse.length - 1; i <= 0; i --){
+//         newWord += nameToReverse[i]
+//     }return newWord
+
+// }
+
+// reversed(superName)
+
+
+// FUNCTION 1 
+// --------------------------------------------------------------------
+
+function tablas(){
+    for(i = 0; i<= 10 ; i++){
+        console.log(`Tabla del ${i}`)
+        for(j = 0; j <= 10 ; j ++){
+            console.log(`${i} * ${j} = ${i*j}`)
+        }
+    }
 }
+
+tablas()
+
+//FUNCTION 2
+// --------------------------------------------------------------------
+
+function sumaN (newNum){
+    let resultado = 0
+    for(i = 0; i <= newNum; i++){
+        resultado = resultado + i
+    }
+    return console.log(`la suma total de los numeros del 0 al ${newNum} esigual a ${resultado}`)
+}
+
+sumaN(55)
+
+
+//FUNCTION 4
+// --------------------------------------------------------------------
+
+function sumaDigitos(cantidad){
+    for(i = 0; i < cantidad.length; i++){
+        sumatoria +=  parseInt(cantidad[i])
+    }return console.log(`la suma de los valores de ${digitos} es igual a  ${sumatoria}`)
+}
+
+let digitos = prompt("introduce una cantidad")
+let sumatoria = 0
+
+sumaDigitos(digitos)
+
+
+// FUNCTION 5
+// --------------------------------------------------------------------
+function sumaMultiplos(){
+    let multiplos = 0
+    for (i = 100; i >= 1; i--){
+        if(i % 3 === 0 && i % 5 === 0 ){
+            multiplos = multiplos + i        
+        }
+    }
+    return console.log(`La suma total de los multiplos es ${multiplos}`)
+}
+
+sumaMultiplos()
+
+// --------------------------------------------------------------------
+
+//Parametros por Default
 
 /*
-Pedir al usuario un STRING
-Contar las Vocales
-Imprimir en consla el numero de vocales y consonantes
-2 vocales y 2 consonantes
+funtcion sunNumbers(a, b, c = 5){
+    let suma = a +b +c
+    }return suma
+
+sumNumbers(10, 15)
 */
-
-let word = prompt("Introduce una palabra u oración").toLowerCase()
-let vocalCount = 0;
-
-for(i=0 ; i<=word.length; i++){
-    if(word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u"){
-        vocalCount = vocalCount +1
-    }
-    
-}
-console.log(`La palabra ${word} tiene ${vocalCount} vocales y ${word.length - vocalCount} consonantes`)
-
-
-
-
-
-// EJERCICIO #1
-
-let word = prompt("Introduce una palabra u oración").toLowerCase()
-let vocalCount = 0;
-let spaces = 0;
-
-while(word.length <= 0){
-   word = prompt("Introduce una palabra u oración valida").toLowerCase()
-}
-
-
-for(i=0; i<=word.length; i++){
-    if(word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u"){
-        vocalCount = vocalCount + 1
-    }else if (word[i] == " "){
-        spaces = spaces + 1
-    }else{
-    }
-}
-console.log(`La palabra ${word} tiene ${vocalCount} vocales y ${word.length - vocalCount - spaces} consonantes`)
-
-
-
-
-
-/* 
-EJERCICIO #2
-Pedir una palabra con espacio al inicio y al final 
-Contar las letras a y e
-Contar numero de espacios, sin contar los esapcios al inicio y al final
-*/
-
-let wordSpace = prompt("Introduce una palabra u oración con un espacio inicial y final").toLowerCase().trim()
-let betweenSpaces = 0, vocalEA = 0
-
-while(wordSpace.length <= 0){
-   wordSpace = prompt("Introduce una palabra u oración valida").toLowerCase()
-}
-
- for(let i = 0; i <= wordSpace.length; i++){
-     if(wordSpace[i] == "a"|| wordSpace[i] == "á" || wordSpace[i] == "e" || wordSpace[i] == "é" ){
-        vocalEA = vocalEA + 1
-     }else if(wordSpace[i] == " "){
-         betweenSpaces = betweenSpaces + 1
-     }else{
-     }
- }
- console.log(`La palabra ${wordSpace} tiene ${betweenSpaces} spacios internos y ${vocalEA} vocales e y a `)
