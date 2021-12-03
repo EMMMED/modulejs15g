9f37e91f1197557aa919e24488e1da76c344f6ec
EMMMED <personal.emedina@gmail.com>
2021-12-02 22:35:27 -0600

    Refuerzo clase Seven y Inicio de Metodo Reduce

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,62 +1,129 @@
-/**
- * Escribir un arrow function que invierta una oracion
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
- * hint: https://www.w3schools.com/jsref/jsref_join.asp
- */
+// //TAREA HW
 
-const word = prompt("Introduce una frase")
-
-const wordReverse = word =>{
-    let wordReversed = ""
-    for(i = word.length -1; i >= 0; i--){
-        wordReversed = wordReversed + word[i]
-    }
-    console.log(wordReversed)
-}
+// // 1. Generar una function
+// // que reciba un array como parametro
+// // y devuelva solo los items que son pares
+// // p.ej. onlyEvenValues( [1, 2, 3, 4] )
+// // -> [2, 4]
+// // Aplicar el metodo .forEach()
+// // hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 
 
-/**
- * Escribir un arrow function que compruebe
- * si una oración es un palindromo
- * hint: https://ricardometring.com/javascript-replace-special-characters
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
- */
+// const onlyEvenValues = arrNumbers => {
+//     let arrayToFill = []
+
+//     arrNumbers.forEach(item => {
+//         if(item % 2 === 0){
+//             arrayToFill.push(item)
+//         }
+//     })
+//     console.log(arrayToFill)
+// }
+
+// onlyEvenValues([1,2,3,4,5,6])
 
 
-const palindrome = (pal= 'Isáac no ronca así') => {
-    let normal = pal.normalize('NFD').replace(/[\u300-\u036f]/g,'').toLowerCase().replace(/([^\w]\s+)/g,'')
-    let wr = normal.split('').reverse().join('')
-    return normal == wr ? true :false
-}
+
+// // 2. function que reciba como parametro una array de strings
+// // y devuelva la primer y ultima letra del string
+// // de cada uno de los items del array 
+// // -> firstAndLast (['hola', 'mundo'] )
+// // -> ['ha', 'mo']
+// // .forEach() o .map()
+
+// const firstAndLast = arrStrings => {
+//     let arrayFill = []
+//     arrStrings.map( ( item ) =>{
+//         arrayFill.push(item.slice(0,1) + item.slice(-1))
+//     })
+//     return console.log(arrayFill)
+// }
+
+// firstAndLast(["Emilio" , "Medina", "Pineda"])
 
 
-/**
- * Escribir un arrow function que tome como parametro 2 arrays, 
- * y devuelva un numero con la suma total de esos 2 arrays.
- * arrayReduce( [1,2,3,4], [1,2]) 
- * -> 13
- * 
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
- * 
-*/
+// /**
+//  * 3. Funcion que reciba un array de numeros
+//  * y devuelva el promedio
+//  * -> getAverage( [10, 8, 9, 7] )
+//  * -> 8.5
+//  */
+
+//  const getAverage = (arr) => {
+//     let sumatoria = 0
+//     arr.forEach( (item) => sumatoria += parseInt(item))
+//     return console.log(sumatoria / arr.length)
+// }
+
+// getAverage([10, 10, 10,])
 
 
-const arrayReduce = (arrOne, arrTwo) => {
-    let newArr = arrOne.concat(arrTwo)
-    let arrAdd = 0
+//  4. funcion
+// dado un array de años, filtre los elementos
+// y devuelva un array con los años que son bisiesto
+// convertLeapYear( [1990, 2000, 2001, 2020] )
+// -> [2000,2020]
+// .forEach()
+// hint: https://docs.microsoft.com/es-es/office/troubleshoot/excel/determine-a-leap-year
 
-    newArr.forEach( ( item ) => {
-            arrAdd += item
+
+// const convertLeapYear = arr => {
+//     let arrYearsFiltered = []
+
+//     arrYearsFiltered = arr.filter(year => {
+//         if( (year % 4 === 0 && year % 100 !== 0 ) || (year % 100 === 0 && year % 400 === 0) ){
+//             return year
+//         }
+//     })
+
+//     return arrYearsFiltered
+// }
+
+
+//  EJERCICIO 
+//  MAP
+
+const capitalizer = arrStrings => {
+    let capArrays = [], primera = "", ultima = ""
+    capArrays = arrStrings.map( ( item ) =>{
+        primera = item.slice(0,1).toUpperCase() 
+        ultima = item.slice(1).toLowerCase()
+        return primera + ultima
     })
-    return arrAdd
+    return capArrays
 }
 
+capitalizer(["EMIlio" , "MeDIna", "PIneda"])
 
 
-/**
- * Escribir una función que tome como parametro 2 arrays, y devuelva un array 
- * con los numeros repetidos en los 2 arrays
- * arrayReduce([1,2,3,4],[1,2,5]). 
- * -> [1,2]
-*/
+// FILTER 
+
+// let arrToFilter = [1,2,3,4,5,6,7]
+// let arrFiltered = arrToFilter.filter( (item) => item % 2 === 0)
+// console.log(arrFiltered)
+
+// 
+/*
+["Hola", "mundo", 0, 5]
+1. funcion
+2. Filtrar solo los de tipo String
+ */
+
+
+// EJERCICIO FILTER 
+
+let arrToFilter = ["Hola", "mundo", 0, 5, true, "Koders"]
+// let arrFiltered = arrToFilter.filter( (item) => 
+// {typeof item == 'string'})
+let arrFiltered = arrToFilter.filter( item => typeof item == 'string')
+console.log(arrFiltered)
+
+//  SIntaxis de REduce 
+
+let acumulador = 0
+let arr = [1,2,3]
+
+let suma = arr.reduce((acc, cv, index, arr) => {
+    console.log(acc, cv)
+    return acc + cv
+}, 0)
