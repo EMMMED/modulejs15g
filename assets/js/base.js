b42f6c0f59c48db78714d12c9f550ff1253b7e80
EMMMED <personal.emedina@gmail.com>
2021-12-09 22:37:39 -0600

    DOM Fundaments

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,31 +1,127 @@
-// Seleccionar Elementos 
 
-// Id
-    document.getElementById("head_title")
-    //REgresa el elemento con el id descrito
-// Clase
-    document.getElementsByClassName('li')
-    // Regresa un array con los elementos que cuenten con la clase descrita
-// 
-    // document.querySelector('#head-title').data-title
-    // document.querySelectorAll('')
+// /**
+//  * Selecionar elementos
+//  * 
+//  * 1. Selecionar por ID
+//  */
 
-// Obtener Atributos
+// //  1. Selecionar por ID
+// document.getElementById('head__title') 
+// document.getElementsByTagName('li')
+// document.getElementsByClassName('link__menu')
 
-// Setear
-// Get Atributos
+// document.querySelector('#head__title')
+// document.querySelector('li')
+// document.querySelector('.link__menu')
+// document.querySelectorAll('.link__menu')
+// // document.querySelectorAll('')
 
-    document.getElementById("head_title").getAttribute("data-title")
-    document.getElementById("head_title").getAttribute("data-level")
-// Set Atributos
-    document.querySelector("#head_title").setAttribute('data-custom','random')
-    // Sobrescribir clase
-    document.querySelector("#head_title").setAttribute('class','clase1')
-    // Agregar clase sin sobreescribr las existentes
-    document.querySelector("#head_title").classList.add("clase2", "clase3", "clase4")
-    // Remover Clases 
-    document.querySelector("#head_title").classList.remove("clase3")
+// /**
+//  * Obtener atributos
+//  * Setear atributos
+//  */
 
-    document.querySelector('#head_title').hasAttribute('id')
+// // get
+// let title = document.querySelector('#head__title').getAttribute('data-title')
+// let level = document.querySelector('#head__title').getAttribute('data-level')
 
-//    comprobar si el titulo tiene ID
+// // set
+// document.querySelector('#head__title').setAttribute('data-custom','random')
+// // reemplazando clases 
+// document.querySelector('#head__title').setAttribute('class','trees cuatro')
+
+// // Agregar clases
+// document.querySelector('#head__title').classList.add('clase3')
+// // remover clases
+// document.querySelector('#head__title').classList.remove('clase2')
+
+// // comprobar si existe un atributo
+// document.querySelector('#head__title').hasAttribute('class')
+
+
+// comprobar si el titulo tiene class
+/**
+ * Si si, comprobar 
+ * ---si tiene la clase "clase1 clase2"
+ * ----- Agregar la "clase 3"
+ * ---sino 
+ * ----- Agregar la clase "custom"
+ */
+
+// let elementTitle = document.querySelector("#head__title")
+
+//  if(elementTitle.hasAttribute("class") == true){
+//     elementTitle.getAttribute("class") == "clase1 clase2" ? elementTitle.classList.add("clase3") :
+//     elementTitle.classList.add("custom")
+//     }
+
+
+    // // createElement('tag')
+    // let ulElement = document.createElement('ul')
+    // ulElement.textContent = 'Text cool'
+
+    // let liFirst = document.createElement('li')
+    // liFirst.textContent = 'item 1'
+
+    // let liSecond = document.createElement('li')
+    // liSecond.textContent = 'item 2'
+
+    // ulElement.appendChild(liFirst)
+    // ulElement.appendChild(liSecond)
+    // document.getElementsByTagName('body')[0].appendChild(ulElement)
+
+
+
+    //--------------------------------------------------------
+
+    let ulMenu = document.createElement('ul')
+    ulMenu.setAttribute("id", "menu")
+    ulMenu.setAttribute("class", "menu")
+    ulMenu.textContent = "Menu"
+
+    let home = document.createElement('li')
+    let products = document.createElement('li')
+    let aboutUs = document.createElement('li')
+
+    home.setAttribute("class", "item__menu")
+    products.setAttribute("class", "item__menu")
+    aboutUs.setAttribute("class", "item__menu")
+
+    home.textContent = 'Home'
+    products.textContent = 'Products'
+    aboutUs.textContent = "About Us"
+
+    ulMenu.appendChild(home)
+    ulMenu.appendChild(products)
+    ulMenu.appendChild(aboutUs)
+    // document.getElementsByTagName('li').setAttribute("class", "item_menu")
+
+    // document.querySelector('#head__title').setAttribute('class','trees cuatro')
+    document.getElementsByTagName('body')[0].appendChild(ulMenu)
+
+    
+
+    // document.getElementsByTagName("li").add('class', 'item_menu')
+    
+
+//     <ul id="menu" class="menu">
+//     <li class="item__menu">Home</li>
+//     <li class="item__menu">Products</li>
+//     <li class="item__menu">About Us</li>
+//     </ul>
+
+
+
+// data attributes
+// propiedades personalizadas
+
+
+
+/**
+ * Seleccionar padres e hijos (traversing)
+ */
+
+
+/**
+ * Manipular elementos del DOM
+ */
